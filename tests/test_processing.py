22882a0ddc89503e333ae2636e0d98435d93194f
Alister Trabattoni <alister.trabattoni@gmail.com>
2023-06-26 15:50:23 +0200

    Add test to integrate.

---
+++ tests/test_processing.py
@@ -0,0 +1,57 @@
+import dask.array as da
+import numpy as np
+import scipy.signal as sp
+
+from xdas.core import Coordinate, Database
+from xdas.processing import LFilter, SignalProcessingChain, SOSFilter
+
+
+class TestProcessing:
+    def test_processing(self):
+        fs = 125
+        ks = 1 / 10
+        data = da.random.normal(size=(1000, 100))
+        time = Coordinate([0, data.shape[0] - 1], [0.0, (data.shape[0] - 1) / fs])
+        distance = Coordinate([0, data.shape[1] - 1], [0.0, (data.shape[1] - 1) / ks])
+        xarr = Database(data, {"time": time, "distance": distance})
+        b, a = sp.iirfilter(4, 0.5, btype="lowpass")
+        lfilter = LFilter(b, a, "time")
+        result_direct = lfilter(xarr)
+        chunk_size = 100
+        lfilter.reset()
+        result_chunks = xarr.copy()
+        for k in range(xarr.shape[0] // chunk_size):
+            query = {"time": slice(k * chunk_size, (k + 1) * chunk_size)}
+            result_chunks[query] = lfilter(xarr[query]).data
+        lfilter.reset()
+        chain = SignalProcessingChain([lfilter])
+        out = chain.process(xarr, "time", chunk_size, parallel=False)
+        out = np.concatenate([x.data for x in out])
+        lfilter.reset()
+        assert chain.filters[0].zi == None
+        out_parallel = chain.process(xarr, "time", chunk_size, parallel=True)
+        out_parallel = np.concatenate([x.data for x in out_parallel])
+        assert np.allclose(result_chunks.data, result_direct.data)
+        assert np.allclose(out, result_direct.data)
+        assert np.allclose(out_parallel, result_direct.data)
+
+        sos = sp.iirfilter(4, 0.5, btype="lowpass", output="sos")
+        sosfilter = SOSFilter(sos, "time")
+        result_direct = sosfilter(xarr)
+        chunk_size = 100
+        sosfilter.reset()
+        result_chunks = xarr.copy()
+        for k in range(xarr.shape[0] // chunk_size):
+            query = {"time": slice(k * chunk_size, (k + 1) * chunk_size)}
+            result_chunks[query] = sosfilter(xarr[query]).data
+        sosfilter.reset()
+        chain = SignalProcessingChain([sosfilter])
+        out = chain.process(xarr, "time", chunk_size, parallel=False)
+        out = np.concatenate([x.data for x in out])
+        sosfilter.reset()
+        assert chain.filters[0].zi == None
+        out_parallel = chain.process(xarr, "time", chunk_size, parallel=True)
+        out_parallel = np.concatenate([x.data for x in out_parallel])
+        assert np.allclose(result_chunks.data, result_direct.data)
+        assert np.allclose(out, result_direct.data)
+        assert np.allclose(out_parallel, result_direct.data)
--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,57 +1,14 @@
-import dask.array as da
 import numpy as np
-import scipy.signal as sp
+import xarray as xr
 
-from xdas.core import Coordinate, Database
-from xdas.processing import LFilter, SignalProcessingChain, SOSFilter
+import xdas.signal
 
 
-class TestProcessin:
-    def test_processing(self):
-        fs = 125
-        ks = 1 / 10
-        data = da.random.normal(size=(1000, 100))
-        time = Coordinate([0, data.shape[0] - 1], [0.0, (data.shape[0] - 1) / fs])
-        distance = Coordinate([0, data.shape[1] - 1], [0.0, (data.shape[1] - 1) / ks])
-        xarr = Database(data, {"time": time, "distance": distance})
-        b, a = sp.iirfilter(4, 0.5, btype="lowpass")
-        lfilter = LFilter(b, a, "time")
-        result_direct = lfilter(xarr)
-        chunk_size = 100
-        lfilter.reset()
-        result_chunks = xarr.copy()
-        for k in range(xarr.shape[0] // chunk_size):
-            query = {"time": slice(k * chunk_size, (k + 1) * chunk_size)}
-            result_chunks[query] = lfilter(xarr[query]).data
-        lfilter.reset()
-        chain = SignalProcessingChain([lfilter])
-        out = chain.process(xarr, "time", chunk_size, parallel=False)
-        out = np.concatenate([x.data for x in out])
-        lfilter.reset()
-        assert chain.filters[0].zi == None
-        out_parallel = chain.process(xarr, "time", chunk_size, parallel=True)
-        out_parallel = np.concatenate([x.data for x in out_parallel])
-        assert np.allclose(result_chunks.data, result_direct.data)
-        assert np.allclose(out, result_direct.data)
-        assert np.allclose(out_parallel, result_direct.data)
-
-        sos = sp.iirfilter(4, 0.5, btype="lowpass", output="sos")
-        sosfilter = SOSFilter(sos, "time")
-        result_direct = sosfilter(xarr)
-        chunk_size = 100
-        sosfilter.reset()
-        result_chunks = xarr.copy()
-        for k in range(xarr.shape[0] // chunk_size):
-            query = {"time": slice(k * chunk_size, (k + 1) * chunk_size)}
-            result_chunks[query] = sosfilter(xarr[query]).data
-        sosfilter.reset()
-        chain = SignalProcessingChain([sosfilter])
-        out = chain.process(xarr, "time", chunk_size, parallel=False)
-        out = np.concatenate([x.data for x in out])
-        sosfilter.reset()
-        assert chain.filters[0].zi == None
-        out_parallel = chain.process(xarr, "time", chunk_size, parallel=True)
-        out_parallel = np.concatenate([x.data for x in out_parallel])
-        assert np.allclose(result_chunks.data, result_direct.data)
-        assert np.allclose(out, result_direct.data)
-        assert np.allclose(out_parallel, result_direct.data)
+class TestSigal:
+    def test_integrate(self):
+        n = 100
+        d = 5.0
+        s = (d / 2) + d * np.arange(n)
+        da = xr.DataArray(data=np.ones(n), coords={"distance": s})
+        da = xdas.signal.integrate(da, midpoints=True)
+        assert np.allclose(da, da["distance"])
