fe36500abbc2ac4e0241ea489e085474e9bdce8a
Martijn van den Ende <martijnende@gmail.com>
2024-02-16 15:48:09 +0100

    Added tests for stateless and statefull processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,37 +1,110 @@
+import os
 import tempfile
 
 import numpy as np
 import scipy.signal as sp
 
+import xdas
 from xdas.core import Database
-from xdas.processing import ProcessingChain, SOSFilter, DatabaseLoader, DatabaseWriter
+from xdas.synthetics import generate
+from xdas.processing import (
+    ProcessingChain, SOSFilter, DatabaseLoader, DatabaseWriter
+)
 
 
 class TestProcessing:
-    def generate(self):
-        fs = 10
-        ks = 1 / 10
-        data = np.random.randn(1000, 100)
-        return Database(
-            data,
-            {
-                "time": {
-                    "tie_indices": [0, data.shape[0] - 1],
-                    "tie_values": np.array(
-                        [round(0.0), round(1e9 * (data.shape[0] - 1) / fs)],
-                        dtype="datetime64[ns]",
-                    ),
-                },
-                "distance": {
-                    "tie_indices": [0, data.shape[1] - 1],
-                    "tie_values": [0.0, (data.shape[1] - 1) / ks],
-                },
-            },
-        )
 
-    def test_all(self):
+    def test_sequence(self):
+        """
+        Objective: to test a sequence of NumPy functions
+        """
+
         with tempfile.TemporaryDirectory() as tempdir:
-            db = self.generate()
+            
+            # Generate a temporary dataset
+            generate(tempdir)            
+
+            # Load the database
+            db = xdas.open_database(os.path.join(tempdir, "sample.nc"))
+
+            # Wrapper for numpy.mean
+            def my_mean(db, dim):
+                dim = xdas.signal.parse_dim(db, dim)
+                axis = db.get_axis_num(dim)
+                result = np.mean(db.values, axis=axis)
+                return db.copy(data=result)
+
+            # Sequence to execute
+            sequence = xdas.Sequence(
+                xdas.Atom(np.abs),
+                xdas.Atom(np.square, name="some square"),
+                xdas.Atom(my_mean, dim="time"),
+            )
+
+            # Process using sequence.execute
+            result1 = sequence.execute(db)
+            # Process manually
+            result2 = my_mean(np.abs(db)**2, dim="time")
+
+            # Check that automatic and manual results are the same
+            assert np.allclose(result1.values, result2.values)
+    
+    def test_stateful(self):
+        """
+        Objective: to test state updates of the StateAtom class
+        """
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            
+            # Generate a temporary dataset
+            generate(tempdir)            
+
+            # Load the database
+            db = xdas.open_database(os.path.join(tempdir, "sample.nc"))
+
+            # Initialise the SOS filter
+            corners = 4
+            freq = 1.0
+            btype = "lowpass"
+            fs = 50.
+            sos = sp.iirfilter(corners, freq, btype=btype, ftype="butter", output="sos", fs=fs)
+            zi = np.zeros((sos.shape[0], 2, db.shape[1]))
+
+            # Wrapper around the SciPy sosfilt
+            def sosfilter(db, zi, sos, dim="last"):
+                dim = xdas.signal.parse_dim(db, dim)
+                axis = db.get_axis_num(dim)
+                result, zi = sp.sosfilt(sos, db.values, axis=axis, zi=zi)
+                return db.copy(data=result), zi
+
+            # Sequence to execute
+            sequence = xdas.Sequence(
+                xdas.StateAtom(
+                    sosfilter, sos=sos,
+                    state_arg="zi", state=zi.copy(), 
+                    dim="time", name="sosfilt"
+                ),
+            )
+
+            # Get the result for the entire dataset processed at once
+            # Make sure to use .copy() to prevent state updates 
+            # associated with the StateAtom
+            result1 = sequence.copy().execute(db)
+            # Initialise the data loader/writer
+            data_loader = xdas.processing.DatabaseLoader(db, chunks={"time": 100})
+            data_writer = xdas.processing.DatabaseWriter(tempdir)
+            # Perform chunked processing
+            result2 = sequence.copy().execute(data_loader, data_writer)
+
+            # Check that the chunked results are the same as the monolithic result
+            assert np.allclose(result1.values, result2.values)
+
+
+
+    def test_all_old(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            generate(tempdir)            
+            db = xdas.open_database(os.path.join(tempdir, "sample.nc"))
             dim = "time"
 
             data_loader = DatabaseLoader(db, {dim: 1000})
