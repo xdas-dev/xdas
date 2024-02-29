27908d75b26d73c48e6ffd0e84f9024f1423576a
ClaudioStrumia <120019053+ClaudioStrumia@users.noreply.github.com>
2024-02-29 15:52:14 +0100

    Created medfilt (#8)
    * Created medfilt
    
    * Add test for medfilt
    
    * Added medfilt and medfilt_test
    
    * Updated medfilt and test_medfilt
    
    * Updated test_medfilt
    
    * Updated test_medfilt
    
    * Merged dev
    
    * merged dev
    
    * Update test_signal.py
    
    * Clean all.
    
    ---------
    
    Co-authored-by: Alister Trabattoni <alister.trabattoni@gmail.com>

--- xdas/signal.py
+++ xdas/signal.py
@@ -384,6 +384,56 @@ def sliding_mean_removal(db, wlen, window="hann", pad_mode="reflect", dim="last"
     return db.copy(data=data)
 
 
+def medfilt(db, kernel_dim):
+    """
+    Perform a median filter along given dimensions
+
+    Apply a median filter to the input using a local window-size given by kernel_size.
+    The array will automatically be zero-padded.
+
+    Parameters
+    ----------
+    db : Database
+        A database to filter.
+    kernel_dim : dict
+        A dictionary which keys are the dimensions over which to apply a median
+        filtering and which values are the related kernel size in that direction.
+        All values must be odd. If not all dims are provided, missing dimensions
+        are associated to 1, i.e. no median filtering along that direction.
+        At least one dimension must be passed.
+
+    Returns
+    -------
+    Database
+        The median filtered data.
+
+    Examples
+    --------
+    A median filter is applied to some synthetic database with a median window size
+    of 7 along the time dimension and 5 along the space dimension.
+    >>> import xdas.signal as xp
+    >>> from xdas.synthetics import generate
+    >>> db = generate()
+    >>> xp.medfilt(db, {"time": 7, "distance": 5})
+    <xdas.Database (time: 300, distance: 401)>
+    array([[0., 0., 0., ..., 0., 0., 0.],
+           [0., 0., 0., ..., 0., 0., 0.],
+           [0., 0., 0., ..., 0., 0., 0.],
+           ...,
+           [0., 0., 0., ..., 0., 0., 0.],
+           [0., 0., 0., ..., 0., 0., 0.],
+           [0., 0., 0., ..., 0., 0., 0.]])
+    Coordinates:
+      * time (time): 2023-01-01T00:00:00.000 to 2023-01-01T00:00:05.980
+      * distance (distance): 0.000 to 10000.000
+    """
+    if not all(dim in db.dims for dim in kernel_dim.keys()):
+        raise ValueError("dims provided not in database")
+    kernel_size = tuple(kernel_dim[dim] if dim in kernel_dim else 1 for dim in db.dims)
+    data = sp.medfilt(db.values, kernel_size)
+    return db.copy(data=data)
+
+
 def fft(db, n=None, dim={"last": "frequency"}, norm=None):
     ((olddim, newdim),) = dim.items()
     olddim = parse_dim(db, olddim)
