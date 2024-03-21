3bc6401c3a78b2a514464fa514f99d1481965953
Alister Trabattoni <alister.trabattoni@gmail.com>
2024-03-21 12:15:10 +0100

    Add todo test.

--- tests/test_core.py
+++ tests/test_core.py
@@ -27,6 +27,8 @@ class TestCore:
             },
         )
 
+    def test_open_mfdatacollection(self): ...  # TODO
+
     def test_open_treedatacollection(self):
         with TemporaryDirectory() as dirpath:
             keys = ["LOC01", "LOC02"]
