f5f0f85342ddb7c2bf08a86c0c7aa8421e3e378e
Alister Trabattoni <alister.trabattoni@gmail.com>
2024-03-30 13:46:52 +0100

    Add tests and docstring to virtual.py. WIP: sliceable DataLayout.

--- tests/test_virtual.py
+++ tests/test_virtual.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 
 import numpy as np
@@ -5,68 +6,145 @@ import pytest
 import xarray as xr
 
 import xdas
+from xdas.synthetics import generate
+from xdas.virtual import *
 
 
-class TestAll:
+class TestFunctional:
     def test_all(self):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            # generation
-            shape = (6000, 1000)
-            resolution = (np.timedelta64(10, "ms"), 5.0)
-            starttimes = {
-                "001": np.datetime64("2023-01-01T00:00:00"),
-                "002": np.datetime64("2023-01-01T00:01:00"),
-                "003": np.datetime64("2023-01-01T00:02:00"),
-            }
-            for name, starttime in starttimes.items():
-                db = xdas.Database(
-                    data=np.random.randn(*shape).astype("float32"),
-                    coords={
-                        "time": {
-                            "tie_indices": [0, shape[0] - 1],
-                            "tie_values": [
-                                starttime,
-                                starttime + resolution[0] * (shape[0] - 1),
-                            ],
-                        },
-                        "distance": {
-                            "tie_indices": [0, shape[1] - 1],
-                            "tie_values": [0.0, resolution[1] * (shape[1] - 1)],
-                        },
-                    },
-                )
-                db.to_netcdf(f"{tmpdir}/{name}.nc")
+        with tempfile.TemporaryDirectory() as dirpath:
+            expected = generate()
+            chunks = xdas.chunk(expected, 3)
+            for index, chunk in enumerate(chunks, start=1):
+                chunk.to_netcdf(os.path.join(dirpath, f"{index:03d}.nc"))
 
-            # testing
-            _db = xdas.open_database(f"{tmpdir}/002.nc")
-            _db_loaded = _db.load()
-            _da = _db.to_xarray()
-            datasource = _db.data
-            assert np.allclose(np.asarray(datasource[0]), _da.values[0])
-            assert np.allclose(np.asarray(datasource[0][1]), _da.values[0][1])
-            assert np.allclose(np.asarray(datasource[:, 0][1]), _da.values[:, 0][1])
-            assert np.allclose(np.asarray(datasource[:, 0][1]), _da.values[:, 0][1])
-            assert np.allclose(np.asarray(datasource[10:][1]), _da.values[10:][1])
+            db = xdas.open_database(os.path.join(dirpath, "002.nc"))
+            datasource = db.data
+            assert np.allclose(np.asarray(datasource[0]), db.load().values[0])
+            assert np.allclose(np.asarray(datasource[0][1]), db.load().values[0][1])
+            assert np.allclose(
+                np.asarray(datasource[:, 0][1]), db.load().values[:, 0][1]
+            )
+            assert np.allclose(
+                np.asarray(datasource[:, 0][1]), db.load().values[:, 0][1]
+            )
+            assert np.allclose(np.asarray(datasource[10:][1]), db.load().values[10:][1])
             with pytest.raises(IndexError):
                 datasource[1, 2, 3]
-            assert np.allclose(np.asarray(datasource[10:][1]), _da.values[10:][1])
-            assert np.array_equal(_da.data, _db_loaded.data)
-            db = _db.sel(
-                time=slice("2023-01-01T00:01:20", None),
+            assert np.allclose(np.asarray(datasource[10:][1]), db.load().values[10:][1])
+            assert np.array_equal(db.load().data, db.load().data)
+            db1 = db.sel(
+                time=slice("2023-01-01T00:00:03", None),
+                distance=slice(1000, None),
+            ).load()
+            db2 = db.load().sel(
+                time=slice("2023-01-01T00:00:03", None),
                 distance=slice(1000, None),
             )
-            db = db.isel(
-                time=slice(None, 1000),
-                distance=slice(None, 200),
-            )
-            da1 = db.to_xarray()
-            da = _da.sel(
-                time=slice("2023-01-01T00:01:20", None),
-                distance=slice(1000, None),
-            )
-            da = da.isel(
-                time=slice(None, 1000),
-                distance=slice(None, 200),
-            )
-            da2 = da
-            xr.testing.assert_equal(da1, da2)
+            assert db1.equals(db2)
+
+
+class TestDataLayout:
+    def test_init(self):
+        ...
+
+
+class TestDataSource:
+    def test_init(self):
+        ...
+
+
+class TestSelection:
+    def test_init(self):
+        shape = (2, 3, 5)
+        sel = Selection(shape)
+        assert sel._shape == shape
+        for size, selector in zip(shape, sel._selectors):
+            assert isinstance(selector, SliceSelector)
+            assert selector._range == range(size)
+        assert sel._whole == True
+
+    def test_usecases(self):
+        arr = np.arange(2 * 3 * 5).reshape(2, 3, 5)
+        sel = Selection(arr.shape)
+
+        expected = arr[0][:, 1:-1][::2]
+        sub = sel[0][:, 1:-1][::2]
+        slc = sub.get_indexer()
+        assert np.array_equal(arr[slc], expected)
+        assert sub.shape == expected.shape
+        assert sub.ndim == expected.ndim
+
+        expected = arr[:][1:0][:, 1]
+        sub = sel[:][1:0][:, 1]
+        slc = sub.get_indexer()
+        assert np.array_equal(arr[slc], expected)
+        assert sub.shape == expected.shape
+        assert sub.ndim == expected.ndim
+
+
+class TestSingleSelector:
+    def test_init(self):
+        sel = SingleSelector(0)
+        assert sel._index == 0
+
+    def test_get_indexer(self):
+        sel = SingleSelector(0)
+        assert sel.get_indexer() == 0
+
+
+class TestSliceSelector:
+    def test_init(self):
+        sel = SliceSelector(3)
+        assert sel._range == range(3)
+
+    def test_getitem_int(self):
+        sel = SliceSelector(3)
+        assert isinstance(sel[0], SingleSelector)
+        with pytest.raises(IndexError):
+            sel[-4]
+        assert sel[-3]._index == 0
+        assert sel[-2]._index == 1
+        assert sel[-1]._index == 2
+        assert sel[0]._index == 0
+        assert sel[1]._index == 1
+        assert sel[2]._index == 2
+        with pytest.raises(IndexError):
+            sel[3]
+
+    def test_getitem_slice(self):
+        sel = SliceSelector(5)
+        assert isinstance(sel[0:1], SliceSelector)
+        assert sel[:]._range == range(5)
+        assert sel[0:1]._range == range(0, 1)
+        assert sel[1:0]._range == range(1, 0)
+        assert sel._range == range(5)
+        sel = sel[1:-1]
+        assert sel._range == range(1, 4)
+        assert sel[0]._index == 1
+        assert sel[-1]._index == 3
+        sel = sel[::2]
+        assert sel._range == range(1, 4, 2)
+        assert sel[0]._index == 1
+        assert sel[-1]._index == 3
+
+    def test_get_indexer(self):
+        sel = SliceSelector(5)
+        assert sel.get_indexer() == slice(0, 5, 1)
+        assert sel[:].get_indexer() == slice(0, 5, 1)
+        assert sel[0:0].get_indexer() == slice(None, 0, None)
+        assert sel[1:0].get_indexer() == slice(None, 0, None)
+        assert sel[0:1].get_indexer() == slice(0, 1, 1)
+        assert sel[::2].get_indexer() == slice(0, 5, 2)
+        assert sel[:-1].get_indexer() == slice(0, 4, 1)
+        assert sel[-1:].get_indexer() == slice(4, 5, 1)
+        arr = [0, 1, 2, 3, 4]
+        assert arr[:] == arr[sel[:].get_indexer()]
+        assert arr[0:0] == arr[sel[0:0].get_indexer()]
+        assert arr[1:0] == arr[sel[1:0].get_indexer()]
+        assert arr[:1] == arr[sel[:1].get_indexer()]
+        assert arr[1:] == arr[sel[1:].get_indexer()]
+        assert arr[1:4:2] == arr[sel[1:4:2].get_indexer()]
+        assert arr[1:-1] == arr[sel[1:-1].get_indexer()]
+        assert arr[1:-1][1:-1] == arr[sel[1:-1][1:-1].get_indexer()]
+        assert arr[1:-1][::2] == arr[sel[1:-1][::2].get_indexer()]
--- xdas/virtual.py
+++ xdas/virtual.py
@@ -6,9 +6,154 @@ import h5py
 import numpy as np
 
 
+class DataLayout:
+    """
+    A lazy array layout pointing toward multiple netCDF4/HDF5 files.
+
+    Instantiate this class with the final shape of the virtual array. Then Fill it up
+    by assigning to slices of it DataSource objects.
+
+    Once the data assignement is completed, the layout can be virually written into a
+    `h5py.File` or `h5py.Group` object using the `to_dataset` method.
+
+    The DataLayout can be sliced and the selected data accessed without writting the
+    layout to disk. To that end, use `numpy.asarray` or the `__array__` special method.
+    Note that for now, sliced DataLayout cannot be written to disk.
+
+    Parameters
+    ----------
+    shape: tuple of int
+        The shape of the layout.
+    dtype: str of dtype
+        The dtype of the layout.
+    maxshape: tuple of int or None, optional
+        The layout is resizable up to this shape. Use None for axes you want to
+        be unlimited.
+    filename: str, optional
+        The name of the destination file, if known in advance. Mappings from
+        data in the same file will be stored with filename '.', allowing the
+        file to be renamed later.
+
+    Attributes
+    ----------
+    shape: tuple of int or
+        The shape of the layout.
+    dtype: dtype
+        The dtype of the layout.
+    ndim: int
+        The number of dimensions of the layout.
+    nbytes: int
+        The number of bytes virtually linked into the layout.
+
+    Methods
+    -------
+    to_dataset(file_or_group, name)
+        Writes virtually the layout into the specified HDF5 file of group with the
+        given name.
+
+    """
+
+    def __init__(self, shape, dtype, maxshape=None, filename=None):
+        self._layout = h5py.VirtualLayout(shape, dtype, maxshape, filename)
+        self._sel = Selection(self._layout.shape)
+
+    def __array__(self, dtype=None):
+        with TemporaryDirectory() as tmpdirname:
+            fname = os.path.join(tmpdirname, "vds.h5")
+            with h5py.File(fname, "w") as file:
+                dataset = file.create_virtual_dataset(
+                    "__values__", self._layout, fillvalue=np.nan
+                )
+            with h5py.File(fname, "r") as file:
+                dataset = file["__values__"]
+                out = np.asarray(dataset[self._sel.get_indexer()])
+            if dtype is not None:
+                out = out.astype(dtype)
+        return out
+
+    def __repr__(self):
+        return f"DataLayout: {to_human(self.nbytes)} ({self.dtype})"
+
+    def __getitem__(self, key):
+        self = deepcopy(self)
+        self._sel = self._sel.__getitem__(key)
+        return self
+
+    def __setitem__(self, key, value):
+        if not self._sel._whole:
+            raise NotImplementedError("cannot link DataSources to a sliced DataLayout")
+        self._layout.__setitem__(key, value)
+
+    @property
+    def shape(self):
+        return self._layout.shape
+
+    @property
+    def dtype(self):
+        return self._layout.dtype
+
+    @property
+    def ndim(self):
+        return len(self.shape)
+
+    @property
+    def nbytes(self):
+        return np.prod(self.shape) * self.dtype.itemsize
+
+    def to_dataset(self, file_or_group, name):
+        return file_or_group.create_virtual_dataset(
+            name, self._layout, fillvalue=np.nan
+        )
+
+
 class DataSource:
     """
     A lazy array object pointing toward a netCDF4/HDF5 file.
+
+    At creation the array corresponds to an entire file dataset. It can then be
+    sliced to indicate which regions should be used. Sliced DataSource eventually can
+    be assigned to a DataLayout to
+
+    Best practive is to pass it a `h5py.Dataset` obtain destructuring a `h5py.File`.
+    Otherwise the exact filename, dataset name, shape and dtype must be passed.
+
+    The data can be accessed using `numpy.asarray` or the `__array__` special method.
+
+    Parameters
+    ----------
+    path_or_dataset: str or h5py.Dataset
+        The path to a file, or an h5py dataset. If a dataset is given,
+        no other parameters are allowed, as the relevant values are taken from
+        the dataset instead.
+    name: str, optional
+        The name of the source dataset within the file.
+    shape: tuple of int, optional
+        A tuple giving the shape of the dataset.
+    dtype: dtype or str, optional
+        Numpy dtype or string.
+    maxshape: tuple or int or None, optional
+        The source dataset is resizable up to this shape. Use `None` for
+        axes you want to be unlimited.
+
+    Attributes
+    ----------
+    vsource: h5py.VirtualSource
+        The underlying sliced virtual source
+    shape: tuple of int or
+        The shape of the source.
+    dtype: dtype
+        The dtype of the source.
+    ndim: int
+        The number of dimensions of the source.
+    nbytes: int
+        The number of bytes virtually linked into the source.
+
+    Methods
+    -------
+    to_dataset(file_or_group, name)
+        Puts the source into a layout and writes virtuallyit into the specified HDF5
+        file of group with the given name.
+
     """
 
     def __init__(
@@ -25,14 +170,14 @@ class DataSource:
         return self
 
     def __array__(self, dtype=None):
-        return self.to_layout().__array__(dtype)
+        return self._to_layout().__array__(dtype)
 
     def __repr__(self):
         return f"DataSource: {to_human(self.nbytes)} ({self.dtype})"
 
     @property
     def vsource(self):
-        return self._vsource.__getitem__(self._sel.get_key())
+        return self._vsource.__getitem__(self._sel.get_indexer())
 
     @property
     def shape(self):
@@ -43,141 +188,177 @@ class DataSource:
         return self.vsource.dtype
 
     @property
-    def path(self):
-        return self.vsource.path
-
-    @property
-    def name(self):
-        return self.vsource.name
-
-    @property
-    def id(self):
-        return self.vsource.id
-
-    @property
-    def sel(self):
-        return self.vsource.sel
+    def ndim(self):
+        return len(self.vsource.shape)
 
     @property
     def nbytes(self):
         return np.prod(self.shape) * self.dtype.itemsize
 
-    @property
-    def ndim(self):
-        return len(self.vsource.shape)
-
-    def to_layout(self):
+    def _to_layout(self):
         layout = DataLayout(self.shape, self.dtype)
         layout[...] = self.vsource
         return layout
 
     def to_dataset(self, file, name):
-        self.to_layout().to_dataset(file, name)
+        self._to_layout().to_dataset(file, name)
 
 
-class DataLayout(h5py.VirtualLayout):
+class Selection:
     """
-    A composite lazy array pointing toward multiple netCDF4/HDF5 files.
+    Used to perform lazy selection.
+
+    It is usefull when dealing with lazy array to avoid loading unneccessary data.
+    It must be initialized with the shape of the underlying array. It allows to track
+    the succesive slice or single element selections made along the different
+    dimensions of the array. Once the overall selection must be aaplied, the
+    `get_indexer` method can be called to retreive a tuple of slice and/or int that
+    can be applied to the array.
+
+    Parameters
+    ----------
+    shape: tuple of int
+        The shape of the array to slice lazily.
+
+    Attributes
+    ----------
+    shape: tuple of int
+        The shape of the array after selection is applied.
+    ndim: int
+        The dimensionality of the array after selection is applied.
+
+    Methods
+    -------
+    get_indexer()
+        Retrun a tuple of slice or int that can be applied to the array to effectively
+        perform the selection
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from xdas.virtual import Selection
+
+    `Selection` is meant to be used on lazy arrays but here we show an example on a
+    in-memory array for simplicity:
+
+    >>> arr = np.arange(2 * 3 * 5).reshape(2, 3, 5)
+
+    Successive indexing can be computed on the flight:
+
+    >>> expected = arr[0][:, 1:-1][::2]
+
+    Or it can be delayed up to the point where the user is done with indexing:
+
+    >>> sel = Selection(arr.shape)
+    >>> sel = sel[0][:, 1:-1][::2]  # Successive selections
+    >>> slc = sel.get_indexer()
+    >>> assert np.array_equal(arr[slc], expected)
+
+    The shape of the the resulting array can be known in advance:
+
+    >>> assert sel.shape == expected.shape
+
     """
 
-    def __array__(self, dtype=None):
-        with TemporaryDirectory() as tmpdirname:
-            fname = os.path.join(tmpdirname, "vds.h5")
-            with h5py.File(fname, "w") as file:
-                dataset = file.create_virtual_dataset(
-                    "__values__", self, fillvalue=np.nan
-                )
-            with h5py.File(fname, "r") as file:
-                dataset = file["__values__"]
-                out = dataset[...]
-            if dtype is not None:
-                out = out.astype(dtype)
-        return out
-
-    def __repr__(self):
-        return f"DataSource: {to_human(self.nbytes)} ({self.dtype})"
+    def __init__(self, shape):
+        self._shape = shape
+        self._selectors = [SliceSelector(size) for size in shape]
+        self._whole = True
 
     def __getitem__(self, key):
-        raise NotImplementedError(
-            "Cannot slice DataLayout. Use `self.to_netcdf(fname, virtual=True)` to "
-            "write to disk and reopen it with `xdas.open_database(fname)`"
+        sel = deepcopy(self)
+        self._whole = False
+        if not isinstance(key, tuple):
+            key = (key,)
+        dim = 0
+        for k in key:
+            while isinstance(sel._selectors[dim], SingleSelector):
+                dim += 1
+                if dim >= len(self._shape):
+                    raise IndexError(
+                        f"too many indices for array: array is {len(self._shape)}-dimensional"
+                    )
+            sel._selectors[dim] = sel._selectors[dim][k]
+            dim += 1
+        return sel
+
+    @property
+    def shape(self):
+        return tuple(
+            len(selector)
+            for selector in self._selectors
+            if not isinstance(selector, SingleSelector)
         )
 
     @property
     def ndim(self):
         return len(self.shape)
 
-    @property
-    def nbytes(self):
-        return np.prod(self.shape) * self.dtype.itemsize
+    def get_indexer(self):
+        return tuple(selector.get_indexer() for selector in self._selectors)
 
-    def to_dataset(self, file, name):
-        return file.create_virtual_dataset(name, self, fillvalue=np.nan)
+
+class SingleSelector:
+    """
+    A lazy single element selector.
+
+    Parameters
+    ----------
+    index: int
+        The index to select.
+
+    """
+
+    def __init__(self, index):
+        self._index = index
+
+    def get_indexer(self):
+        return self._index
+
+
+class SliceSelector:
+    """
+    A lazy slice selection.
+
+    The actual selection is stored as a `range` object that is transformed to a `slice`
+    object when calling `get_indexer`.
+
+    Parameters
+    ----------
+    size: int
+        The size of the axis of the data to slice lazily.
+
+    """
+
+    def __init__(self, size):
+        self._range = range(size)
+
+    def __getitem__(self, key):
+        item = self._range[key]
+        if isinstance(item, int):
+            return SingleSelector(item)
+        else:
+            sel = deepcopy(self)
+            sel._range = item
+            return sel
+
+    def __len__(self):
+        return len(self._range)
+
+    def get_indexer(self):
+        if len(self) == 0:
+            return slice(0)
+        elif self._range.stop < 0:
+            return slice(self._range.start, None, self._range.step)
+        else:
+            return slice(self._range.start, self._range.stop, self._range.step)
 
 
 def to_human(size):
+    """Convert raw byte numbers into a human readable ones with units."""
     unit = {0: "B", 1: "KB", 2: "MB", 3: "GB", 4: "TB"}
     n = 0
     while size > 1024:
         size /= 1024
         n += 1
     return f"{size:.1f}{unit[n]}"
-
-
-class Selection:
-    def __init__(self, shape):
-        self.shape = shape
-        self.selectors = [SliceSelector(length) for length in shape]
-
-    def __getitem__(self, key):
-        sel = deepcopy(self)
-        if not isinstance(key, tuple):
-            key = (key,)
-        dim = 0
-        for k in key:
-            while isinstance(sel.selectors[dim], SingleSelector):
-                dim += 1
-                if dim >= sel.ndim:
-                    raise IndexError(
-                        f"too many indices for array: array is {sel.ndim}-dimensional"
-                    )
-            sel.selectors[dim] = sel.selectors[dim][k]
-            dim += 1
-        return sel
-
-    @property
-    def ndim(self):
-        return len(self.shape)
-
-    def get_key(self):
-        return tuple(selector.get_key() for selector in self.selectors)
-
-
-class SingleSelector:
-    def __init__(self, index):
-        self.index = index
-
-    def get_key(self):
-        return self.index
-
-
-class SliceSelector:
-    def __init__(self, length):
-        self.range = range(length)
-
-    def __getitem__(self, key):
-        sel = deepcopy(self)
-        item = sel.range[key]
-        if isinstance(item, int):
-            return SingleSelector(item)
-        else:
-            sel.range = item
-            return sel
-
-    def get_key(self):
-        if len(self.range) == 0:
-            return slice(0)
-        elif self.range.stop < 0:
-            return slice(self.range.start, None, self.range.step)
-        else:
-            return slice(self.range.start, self.range.stop, self.range.step)
