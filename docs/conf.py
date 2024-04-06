cd75cb6c7ca3dcb61f6ac0748c693be7b2ab063b
Alister Trabattoni <alister.trabattoni@gmail.com>
2024-04-06 10:27:27 +0200

    Add sphinx.ext.vewcode.

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
