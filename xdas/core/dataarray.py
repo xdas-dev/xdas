Fix ufunc by providing better broadcasting.

xdas/core/dataarray.py
@@ -53,8 +53,11 @@ class DataArray:  # TODO: use numpy.lib.mixins.NDArrayOperatorsMixin
         self._data = data
 
         # coords & dims
-        if coords is None and dims is None:
-            dims = tuple(f"dim_{index}" for index in range(data.ndim))
+        if dims is None:
+            if coords is None:
+                dims = tuple(f"dim_{index}" for index in range(data.ndim))
+            elif isinstance(coords, Coordinates):
+                dims = coords.dims
         if dims is not None and len(dims) != data.ndim:
             raise ValueError("different number of dimensions on `data` and `dims`")
         coords = Coordinates(coords, dims)
@@ -121,31 +124,28 @@ class DataArray:  # TODO: use numpy.lib.mixins.NDArrayOperatorsMixin
             return self.data.__array__(dtype)
 
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
-        from .routines import align  # TODO: circular import
+        from .routines import broadcast_coords, broadcast_to  # TODO: circular import
 
         if not method == "__call__":
             return NotImplemented
-        if len(inputs) > 1 and all(isinstance(value, DataArray) for value in inputs):
-            inputs = align(*inputs)
-        arrays = tuple(
-            value.data if isinstance(value, self.__class__) else value
-            for value in inputs
+
+        coords = broadcast_coords(
+            *tuple(input for input in inputs if isinstance(input, self.__class__))
         )
+        inputs = tuple(broadcast_to(input, coords) for input in inputs)
+
+        arrays = tuple(input.values for input in inputs)
         if "out" in kwargs:
-            kwargs["out"] = tuple(
-                value.data if isinstance(value, self.__class__) else value
-                for value in kwargs["out"]
-            )
+            # TODO: check outputs alignements
+            kwargs["out"] = tuple(np.asarray(output) for output in kwargs["out"])
         if "where" in kwargs:
-            kwargs["where"] = tuple(
-                value.data if isinstance(value, self.__class__) else value
-                for value in kwargs["where"]
-            )
+            kwargs["where"] = np.asarray(broadcast_to(kwargs["where"], coords))
+
         outputs = getattr(ufunc, method)(*arrays, **kwargs)
         if isinstance(outputs, tuple):
-            return tuple(da.copy(data=data) for da, data in zip(inputs, outputs))
+            return tuple(self.__class__(output, coords) for output in outputs)
         else:
-            return inputs[0].copy(data=outputs)
+            return self.__class__(outputs, coords)
 
     def __array_function__(self, func, types, args, kwargs):
         if func not in HANDLED_NUMPY_FUNCTIONS:

xdas/core/routines.py
@@ -9,7 +9,7 @@ import xarray as xr
 from tqdm import tqdm
 
 from ..virtual import VirtualSource, VirtualStack
-from .coordinates import InterpCoordinate, get_sampling_interval
+from .coordinates import Coordinates, InterpCoordinate, get_sampling_interval
 from .dataarray import DataArray
 from .datacollection import DataCollection
 
@@ -701,6 +701,42 @@ def align(*objs):
     Dimensions without coordinates: x
 
     """
+    coords = broadcast_coords(*objs)
+    return tuple(broadcast_to(obj, coords) for obj in objs)
+
+
+def broadcast_coords(*objs):
+    """
+    Broadcasts the coordinates of multiple objects and returns a new Coordinates object.
+
+    Parameters
+    ----------
+    *objs : Variable number of objects with sizes and coordinates.
+
+    Returns
+    -------
+    Coordinates
+        A new Coordinates object with the broadcasted coordinates.
+
+    Raises
+    ------
+    ValueError
+        If the data arrays have incompatible sizes along any dimension or if the
+        coordinates differ between data arrays.
+
+    Examples
+    --------
+    >>> import xdas as xd
+    >>> import numpy as np
+
+    >>> da1 = xd.DataArray(np.arange(2), {"x": [0, 1]})
+    >>> da2 = xd.DataArray(np.arange(3), {"y": [2, 3, 4]})
+    >>> xd.broadcast_coords(da1, da2)
+    Coordinates:
+      * x (x): [0 1]
+      * y (y): [2 ... 4]
+
+    """
     sizes = {}
     coords = {}
     for obj in objs:
@@ -725,11 +761,46 @@ def align(*objs):
             else:
                 coords[name] = coord
     dims = tuple(dim for dim in sizes)
-    out = []
-    for obj in objs:
-        for dim in dims:
-            if dim not in obj.dims:
-                obj = obj.expand_dims(dim)
-        obj = obj.transpose(*dims)
-        out.append(obj)
-    return tuple(out)
+    return Coordinates(coords, dims)
+
+
+def broadcast_to(obj, coords):
+    """
+    Broadcasts an object to match the dimensions specified by the given coordinates.
+
+    Parameters
+    ----------
+    obj : DataArray or array-like
+        The object to be broadcasted.
+    coords : Coordinates
+        The coordinates specifying the dimensions to match.
+
+    Returns
+    -------
+    DataArray
+        The broadcasted object.
+
+    Notes
+    -----
+    - If the input object is not a DataArray, it will be converted to a DataArray using
+      the pro.
+    - The dimensions of the input object will be expanded to match the dimensions
+      specified by the coordinates.
+    - The order of dimensions in the output object will be rearranged to match the
+      order specified by the coordinates.
+
+    """
+    if not isinstance(obj, DataArray):
+        _data = np.asarray(obj)
+        _dims = coords.dims[len(coords.dims) - _data.ndim :]
+        _coords = {
+            name: (coord.dim, coord)
+            for name, coord in coords.items()
+            if coord.dim in _dims
+        }
+        obj = DataArray(_data, _coords, _dims)
+    for dim in coords.dims:
+        if dim not in obj.dims:
+            obj = obj.expand_dims(dim)
+    obj = obj.transpose(*coords.dims)
+    return obj