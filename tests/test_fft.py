1d5207da92bc723de9414a1460b3ff4721b713b3
Alister Trabattoni <alister.trabattoni@gmail.com>
2024-11-06 17:41:45 +0100

    wip

---
+++ tests/test_fft.py
@@ -0,0 +1,9 @@
+import numpy as np
+import xdas as xd
+import xdas.fft as xfft
+
+class TestRFFT:
+    def test_with_non_dimensional(self):
+        da = xd.synthetics.wavelet_wavefronts()
+        da["latitude"] = ("distance", np.arange(da.sizes["distance"]))
+        xfft.rfft(da, dim={"time": "frequency"})
