658d12387290e3e18e87ed0b21cd9cba61de3f6b
Alister Trabattoni <alister.trabattoni@gmail.com>
2024-12-02 16:02:22 +0100

    Add irfft with test and doc.

--- docs/api/fft.md
+++ docs/api/fft.md
@@ -13,4 +13,5 @@
    fft
    ifft
    rfft
+   irfft
 ```
--- tests/test_fft.py
+++ tests/test_fft.py
@@ -11,14 +11,32 @@ class TestRFFT:
         xfft.rfft(da)
 
 
-class TestIFFT:
-    def test_base(self):
+class TestIncerse:
+    def test_standard(self):
         expected = xd.synthetics.wavelet_wavefronts()
         result = xfft.ifft(
-            xfft.fft(expected, dim={"time": "frequency"}), dim={"frequency": "time"}
+            xfft.fft(expected, dim={"time": "frequency"}),
+            dim={"frequency": "time"},
         )
         assert np.allclose(np.real(result).values, expected.values)
         assert np.allclose(np.imag(result).values, 0)
+        for name in result.coords:
+            if name == "time":
+                ref = expected["time"].values
+                ref = (ref - ref[0]) / np.timedelta64(1, "s")
+                ref += result["time"][0].values
+                assert np.allclose(result["time"].values, ref)
+            else:
+                assert result[name].equals(expected[name])
+
+    def test_real(self):
+        expected = xd.synthetics.wavelet_wavefronts()
+        result = xfft.irfft(
+            xfft.rfft(expected, dim={"time": "frequency"}),
+            expected.sizes["time"],
+            dim={"frequency": "time"},
+        )
+        assert np.allclose(result.values, expected.values)
         for name in result.coords:
             if name == "time":
                 ref = expected["time"].values
