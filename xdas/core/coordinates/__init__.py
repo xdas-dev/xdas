e695ab93b0db9940c6429f06ea1fb515610c2aa9
Alister Trabattoni <alister.trabattoni@gmail.com>
2025-12-18 12:18:29 +0100

    Refactoring: separate default module for DefaultCoordinate.

--- xdas/core/coordinates/__init__.py
+++ xdas/core/coordinates/__init__.py
@@ -1,9 +1,9 @@
 from .core import (
     Coordinate,
     Coordinates,
-    DefaultCoordinate,
     get_sampling_interval,
 )
+from .default import DefaultCoordinate
 from .dense import DenseCoordinate
 from .interp import InterpCoordinate
 from .sampled import SampledCoordinate
