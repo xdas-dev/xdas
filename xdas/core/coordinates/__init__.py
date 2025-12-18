d9c82d204b91ffddbb6cc24f238ff9f3f24ffe4a
Alister Trabattoni <alister.trabattoni@gmail.com>
2025-12-18 11:58:31 +0100

    Refactoring: separeate sampled module for SamplesCoordinate.

--- xdas/core/coordinates/__init__.py
+++ xdas/core/coordinates/__init__.py
@@ -3,8 +3,8 @@ from .core import (
     Coordinates,
     DefaultCoordinate,
     DenseCoordinate,
-    SampledCoordinate,
     ScalarCoordinate,
     get_sampling_interval,
 )
 from .interp import InterpCoordinate
+from .sampled import SampledCoordinate
