dde4c57bf19fe20f77a337313eafa3212b9bf248
Alister Trabattoni <alister.trabattoni@gmail.com>
2026-05-09 19:20:37 +0200

    Reduce parametrization.

--- tests/coordinates/test_generic.py
+++ tests/coordinates/test_generic.py
@@ -51,37 +51,36 @@ class TestAppend:
 
 
 class TestGetSplitIndices:
+    CASES = [
+        ("discontinuities", False, [10, 20, 30, 40, 50]),
+        ("discontinuities", None, [20, 30, 40, 50]),
+        ("discontinuities", 1, [20, 30, 40, 50]),
+        ("discontinuities", 2, [40, 50]),
+        ("discontinuities", 4, [40, 50]),
+        ("discontinuities", 8, []),
+        ("discontinuities", 20, []),
+        ("gap", False, [10, 30, 40]),
+        ("gap", None, [30, 40]),  # continuity is a gap
+        ("gap", 1, [30, 40]),
+        ("gap", 2, [40]),
+        ("gap", 4, [40]),
+        ("gap", 8, []),
+        ("gap", 20, []),
+        ("overlap", False, [20, 50]),
+        ("overlap", None, [20, 50]),  # continuity is not an overlap
+        ("overlap", 1, [20, 50]),
+        ("overlap", 2, [50]),
+        ("overlap", 4, [50]),
+        ("overlap", 8, []),
+        ("overlap", 20, []),
+    ]
+
     @pytest.mark.parametrize("ctype", ["interpolated", "sampled"])
     @pytest.mark.parametrize("dtype", [int, float, "datetime64[s]"])
-    @pytest.mark.parametrize(
-        "kind,tolerance,expected",
-        [
-            ("discontinuities", False, [10, 20, 30, 40, 50]),
-            ("discontinuities", None, [20, 30, 40, 50]),
-            ("discontinuities", 1, [20, 30, 40, 50]),
-            ("discontinuities", 2, [40, 50]),
-            ("discontinuities", 4, [40, 50]),
-            ("discontinuities", 8, []),
-            ("discontinuities", 20, []),
-            ("gap", False, [10, 30, 40]),
-            ("gap", None, [30, 40]),  # # continuity is a gap
-            ("gap", 1, [30, 40]),
-            ("gap", 2, [40]),
-            ("gap", 4, [40]),
-            ("gap", 8, []),
-            ("gap", 20, []),
-            ("overlap", False, [20, 50]),
-            ("overlap", None, [20, 50]),  # continuity is not an overlap
-            ("overlap", 1, [20, 50]),
-            ("overlap", 2, [50]),
-            ("overlap", 4, [50]),
-            ("overlap", 8, []),
-            ("overlap", 20, []),
-        ],
-    )
-    def test_generic(self, coord, kind, tolerance, expected):
-        indices = coord.get_split_indices(kind=kind, tolerance=tolerance)
-        np.testing.assert_array_equal(indices, expected)
+    def test_generic(self, coord):
+        for kind, tolerance, expected in self.CASES:
+            indices = coord.get_split_indices(kind=kind, tolerance=tolerance)
+            np.testing.assert_array_equal(indices, expected)
 
     @pytest.mark.parametrize("ctype", ["interpolated", "sampled"])
     def test_wrong_kind(self, ctype):
