d89dad94b70eb0a42f1fbd464fc62959620e689c
Alister Trabattoni <alister.trabattoni@gmail.com>
2026-05-24 21:17:34 +0200

    Invert slow test logic to not skip by default.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,13 @@ def pytest_configure(config):
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--slow", action="store_true", default=False, help="run slow tests"
+        "--skip-slow", action="store_true", default=False, help="skip slow tests"
     )
 
 
 def pytest_collection_modifyitems(config, items):
-    if not config.getoption("--slow"):
-        skip_slow = pytest.mark.skip(reason="slow test, use --slow to run")
+    if config.getoption("--skip-slow"):
+        skip_slow = pytest.mark.skip(reason="slow test, skipped with --skip-slow")
         for item in items:
             if "slow" in item.keywords:
                 item.add_marker(skip_slow)
